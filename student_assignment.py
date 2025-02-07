96c75c239d00a3c1f68c6855a6ad0f603c6b0657
joustonhuang <jouston@gmail.com>
2025-02-07 20:47:15 +0800

    HW01 init commit 1
    Start using PyPDFLoader to load PDF file.

--- student_assignment.py
+++ student_assignment.py
@@ -2,12 +2,21 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import (CharacterTextSplitter,
                                       RecursiveCharacterTextSplitter)
 
+# hw02_01 需要的 import
+from langchain.schema import Document
+
 q1_pdf = "OpenSourceLicenses.pdf"
 q2_pdf = "勞動基準法.pdf"
 
 
 def hw02_1(q1_pdf):
-    pass
+    # Load PDF using PyPDFLoader
+    loader = PyPDFLoader(q1_pdf)
+    doc1 = loader.load()
+
+    # Text Splitter to split text
+    txt_split = CharacterTextSplitter(chunk_overlap=0) 
+    # pass
 
 def hw02_2(q2_pdf):
     pass
